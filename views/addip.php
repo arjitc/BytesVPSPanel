157edd3366ee6ce48af824e2e71c83d661183659
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-10 01:02:26 +0530

    Update addip.php

--- views/addip.php
+++ views/addip.php
@@ -3,10 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
-
+ 
     <title>Static Top Navbar Example for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
